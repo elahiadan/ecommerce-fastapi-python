08e4a36332cd83accf751d8a5e753a4888b2c7d1
Noor Shan <elahiadan7@gmail.com>
2026-09-19 13:17:06 +0530

    this is ecommerce system apis using fastapi

--- app/config.py
+++ app/config.py
@@ -28,7 +28,7 @@ class Settings:
     )
     access_token_expire_minutes: int = field(
         default_factory=lambda: int(
-            os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60")
+            os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES") or "60"
         )
     )
 
